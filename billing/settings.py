9327f42bdef8d77358ca740ec7355f9f3047e85c
Leisy17 <leisysado@gmail.com>
2024-10-13 20:44:26 -0500

    Add config migrations

--- billing/settings.py
+++ billing/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Application definition
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
+    'drf_yasg',
     'stock',
     'customers',
     'users',
--- stock/serializers.py
+++ stock/serializers.py
@@ -2,16 +2,19 @@ from rest_framework import serializers
 from .models import Product, Stock
 from warehouse.serializers import WarehouseSerializer
 
-
 class ProductSerializer(serializers.ModelSerializer):
-    stock = WarehouseSerializer(read_only=True)
+    stock = WarehouseSerializer(read_only=True)  # Custom field declared
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'warehouse']
-
+        fields = ['id', 'name', 'description', 'price', 'warehouse', 'stock']  # Add 'stock' here
 
 class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
         fields = ['id', 'product', 'amount', 'warehouse']
+
+    def get_stock(self, obj):
+        # Example logic to fetch stock data
+        stock = Stock.objects.filter(product=obj).first()
+        return StockSerializer(stock).data if stock else None
